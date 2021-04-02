edf381792993738fea8aa0c4b476ea860548b5f9
Guillermo Battistel <guillermo.battistel@gmail.com>
2021-04-02 18:44:58 -0300

    Fix logger comments

--- GBSwiftLogger/Logger.swift
+++ GBSwiftLogger/Logger.swift
@@ -18,11 +18,17 @@ import Foundation
 -- `all`:  Will print all level of messages
 -*/
 public enum LoggerLevels: Int {
+    /// When the `verboseLevel` is set to `none`, all logs level al disable (no log is printed)
     case none = 0
+    /// When the `verboseLevel` is set to `error`, only `error` logs will be printed
     case error
+    /// When the `verboseLevel` is set to `warning`, only `warning` logs will be printed
     case warning
+    /// When the `verboseLevel` is set to `success`, only `success` logs will be printed
     case success
+    /// When the `verboseLevel` is set to `info`, only `info` logs will be printed
     case info
+    /// When the `verboseLevel` is set to `info`,   All the logs will be printed
     case all
 }
 
@@ -31,7 +37,9 @@ public class Logger {
 
     public static let log = Logger()
 
-    /// represents the current log level: `all` is set as default
+    /// Represents the current log level: `all` is set as default
+    /// You can disable all logs by doing: `verboseLevel = .none`
+    /// For more information check `LoggerLevels` enum
     public var verboseLevel: LoggerLevels = .all
 
     private var errorGlyph: String = "\u{1F6AB}"    // Glyph for messages of level .Error
@@ -43,6 +51,7 @@ public class Logger {
 
 
     /// Prints information messages if `verboseLevel` is set to `.all` or `.info`
+    /// Example of usage: `Logger.log.i(message: "My info log")`
     /// - Parameter message: message to be printed
     public func i(message: String) {
         if verboseLevel == .all || verboseLevel == .info {
@@ -51,6 +60,7 @@ public class Logger {
     }
 
     /// Prints Error messages if `verboseLevel` is set to `.all` or `.error`
+    /// Example of usage: `Logger.log.e(message: "My error log")`
     /// - Parameter message: message to be printed
     public func e(message: String) {
         if verboseLevel == .all || verboseLevel == .error {
@@ -59,6 +69,7 @@ public class Logger {
     }
 
     /// Prints Warning messages if `verboseLevel` is set to `.all` or `.warning`
+    /// Example of usage: `Logger.log.w(message: "My warning log")`
     /// - Parameter message: message to be printed
     public func w(message: String) {
         if verboseLevel == .all || verboseLevel == .warning {
@@ -67,6 +78,7 @@ public class Logger {
     }
 
     /// Prints Success messages if `verboseLevel` is set to `.all` or `.success`
+    /// Example of usage: `Logger.log.s(message: "My success log")`
     /// - Parameter message: message to be printed
     public func s(message: String) {
         if verboseLevel == .all || verboseLevel == .success {
