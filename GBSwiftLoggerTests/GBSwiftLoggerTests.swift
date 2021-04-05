0782e70e5bfe4452afac070307988d0660695b78
Guillermo Battistel <guillermo.battistel@gmail.com>
2021-04-05 20:53:43 -0300

    Fix user environment variable

--- GBSwiftLoggerTests/GBSwiftLoggerTests.swift
+++ GBSwiftLoggerTests/GBSwiftLoggerTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 class GBSwiftLoggerTests: XCTestCase {
 
-    let environmentUser = "<ENV_USER"
+    let environmentUser = "<ENV_USER>"
     let environmentPassword = "<ENV_PASSWORD>"
     let environmentToken = "<ENV_TOKEN>"
 
