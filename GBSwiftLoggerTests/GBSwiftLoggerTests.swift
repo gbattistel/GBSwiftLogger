c306f7d510857aa8c6f352a3829265c282c2ebfa
Guillermo Battistel <guillermo.battistel@gmail.com>
2021-04-05 20:46:21 -0300

    Bash script to set environment variables from GitHub secrets

--- GBSwiftLoggerTests/GBSwiftLoggerTests.swift
+++ GBSwiftLoggerTests/GBSwiftLoggerTests.swift
@@ -10,28 +10,51 @@ import XCTest
 
 class GBSwiftLoggerTests: XCTestCase {
 
-    let environmentVariable = "<ENV_VARIABLE>"
+    let environmentUser = "<ENV_USER"
+    let environmentPassword = "<ENV_PASSWORD>"
+    let environmentToken = "<ENV_TOKEN>"
+
     func testInfoLog() {
         Logger.log.i(message: "My info log")
-        Logger.log.i(message: "My environment variable: \(environmentVariable)")
-        XCTAssertEqual(environmentVariable, "MY_SECRET_ENV_VARIABLE")
+        //Check github workflow and setup_env.sh script
+        Logger.log.i(message: "My environment user: \(environmentUser)")
+        XCTAssertEqual(environmentUser, "MY_SECRET_ENV_USER")
+        Logger.log.i(message: "My environment user: \(environmentPassword)")
+        XCTAssertEqual(environmentPassword, "MY_SECRET_ENV_PASSWORD")
+        Logger.log.i(message: "My environment user: \(environmentToken)")
+        XCTAssertEqual(environmentToken, "MY_SECRET_ENV_TOKEN")
     }
 
     func testLog() {
         Logger.log.e(message: "My error log")
-        Logger.log.e(message: "My environment variable: \(environmentVariable)")
-        XCTAssertEqual(environmentVariable, "MY_SECRET_ENV_VARIABLE")
+        //Check github workflow and setup_env.sh script
+        Logger.log.e(message: "My environment user: \(environmentUser)")
+        XCTAssertEqual(environmentUser, "MY_SECRET_ENV_USER")
+        Logger.log.e(message: "My environment user: \(environmentPassword)")
+        XCTAssertEqual(environmentPassword, "MY_SECRET_ENV_PASSWORD")
+        Logger.log.e(message: "My environment user: \(environmentToken)")
+        XCTAssertEqual(environmentToken, "MY_SECRET_ENV_TOKEN")
     }
 
     func testWarningLog() {
         Logger.log.w(message: "My warning log")
-        Logger.log.w(message: "My environment variable: \(environmentVariable)")
-        XCTAssertEqual(environmentVariable, "MY_SECRET_ENV_VARIABLE")
+        //Check github workflow and setup_env.sh script
+        Logger.log.w(message: "My environment user: \(environmentUser)")
+        XCTAssertEqual(environmentUser, "MY_SECRET_ENV_USER")
+        Logger.log.w(message: "My environment user: \(environmentPassword)")
+        XCTAssertEqual(environmentPassword, "MY_SECRET_ENV_PASSWORD")
+        Logger.log.w(message: "My environment user: \(environmentToken)")
+        XCTAssertEqual(environmentToken, "MY_SECRET_ENV_TOKEN")
     }
 
     func testSuccessLog() {
         Logger.log.s(message: "My success log")
-        Logger.log.s(message: "My environment variable: \(environmentVariable)")
-        XCTAssertEqual(environmentVariable, "MY_SECRET_ENV_VARIABLE")
+        //Check github workflow and setup_env.sh script
+        Logger.log.s(message: "My environment user: \(environmentUser)")
+        XCTAssertEqual(environmentUser, "MY_SECRET_ENV_USER")
+        Logger.log.s(message: "My environment user: \(environmentPassword)")
+        XCTAssertEqual(environmentPassword, "MY_SECRET_ENV_PASSWORD")
+        Logger.log.s(message: "My environment user: \(environmentToken)")
+        XCTAssertEqual(environmentToken, "MY_SECRET_ENV_TOKEN")
     }
 }
