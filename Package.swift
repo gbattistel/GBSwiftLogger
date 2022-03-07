b23f311e866a7f0f1409b9837ac301288231aff1
Guillermo Battistel <guillermo@telnyx.com>
2022-03-07 18:41:03 -0300

    Fix: xcframework checksum

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             .binaryTarget(
                      name: "GBSwiftLogger",
                      url: "https://github.com/gbattistel/GBSwiftLogger/releases/download/1.0.6/GBSwiftLogger.xcframework.zip",
-                     checksum: "5fcf8acd2fd04a4b561306e55ff92d462d73cd2763b5b2062505d07a311fdaa7"
+                     checksum: "98fddcf173410bf5f641016a608a717580246e423147f22e0fa51da35871c54b"
              )
         ]
 )
