9deba50fe6e6e1c6e77a2afdda22ab00d822e21b
Guillermo Battistel <guillermo@telnyx.com>
2022-03-07 17:12:14 -0300

    Use xcframework v 1.0.6

--- Package.swift
+++ Package.swift
@@ -4,11 +4,15 @@ import PackageDescription
 let package = Package(
         name: "GBSwiftLogger",
         products: [
-            .library(name: "GBSwiftLogger", targets: ["GBSwiftLogger"])
+            .library(name: "GBSwiftLogger",
+                     targets: ["GBSwiftLogger"])
         ],
         dependencies: [],
         targets: [
-            .target(name: "GBSwiftLogger",
-                    path: "GBSwiftLogger")
+            .binaryTarget(
+                     name: "GBSwiftLogger",
+                     url: "https://github.com/gbattistel/GBSwiftLogger/releases/download/1.0.6/GBSwiftLogger.xcframework.zip",
+                     checksum: "5fcf8acd2fd04a4b561306e55ff92d462d73cd2763b5b2062505d07a311fdaa7"
+             )
         ]
 )
